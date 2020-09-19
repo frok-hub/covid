b92b052c6a5ca706719fbc62396a3e206ceddb27
frok-hub <69907864+frok-hub@users.noreply.github.com>
2020-09-19 18:56:09 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+from covid_india import states
+
+ 
+
+ap = states.getdata('Andhra Pradesh')
+
+ 
+
+print('Total Covid Cases:',ap['Total'])
+
+ 
+
+print('Cured Cases:',ap['Cured'])
+
+ 
+
+print('Death Cases:',ap['Death'])
